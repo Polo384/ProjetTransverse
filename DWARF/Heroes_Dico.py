93937d5ee2b5dfdded4592ec4253d2de5d3f68da
ANDR3LAXE <117640071+ANDR3LAXE@users.noreply.github.com>
2023-02-01 14:12:05 +0000

    Delete DWARF directory

--- DWARF/Heroes_Dico.py
+++
@@ -1,7 +0,0 @@
-Skeleton = {
-    'Skeleton/Skeleton_Hit.png':8,
-    'Skeleton/Skeleton_Idle.png':11,
-    'Skeleton/Skeleton_Walk.png':13,
-    'Skeleton/Skeleton_Dead.png':15,
-    'Skeleton/Skeleton_Attack.png':18,
-    'Skeleton/Skeleton_React.png':4}
