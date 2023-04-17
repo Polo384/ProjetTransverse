1b64a56c9b0ceed786b39f5964844acc71653ab1
AlikBook <117640028+AlikBook@users.noreply.github.com>
2023-04-17 13:18:11 +0200

    Update main.py

--- DWARF/main.py
+++ DWARF/main.py
@@ -2,6 +2,7 @@ import pygame, ctypes, sys
 from functions import store_animations
 from Heroes_Dico import *
 from level import Level
+from Menu_classes import Menu
 from settings import screen_width, screen_height, FPS, level_map
     
 ctypes.windll.user32.SetProcessDPIAware() 
@@ -23,17 +24,26 @@ def display_fps():
 
 all_animations = store_animations([santa_dico_v1, minotaur_dico_v1, dwarf_dico_v1, indiana_jones_dico_v1, adventurer_dico_v1, bat_dico_v1, halo_dico_v1, gladiator_dico_v1, demon_dico_v1, cyclop_dico_v1], [hobbit_dico_v2, question_mark_dico_v2])
 
-player1_hero_choice = 'santa'
-player2_hero_choice = 'hobbit'
-
-player1_hero = [all_animations[heroes_dico[player1_hero_choice][0]], heroes_dico[player1_hero_choice][1], heroes_dico[player1_hero_choice][2], player1_hero_choice, heroes_dico[player1_hero_choice][3]]
-player2_hero = [all_animations[heroes_dico[player2_hero_choice][0]], heroes_dico[player2_hero_choice][1], heroes_dico[player2_hero_choice][2], player2_hero_choice, heroes_dico[player2_hero_choice][3]]
-
-level = Level(level_map, screen, player1_hero, player2_hero)
-
-while True:
-    screen.fill((255, 255, 255))
-    level.run()
-    display_fps()
+MAIN_menu = Menu(all_animations)
+player1_hero_choice = "none"
+player2_hero_choice = "none"
+start = False
+create_level = True
+game_start_variable = True
+while game_start_variable:
+    if(start == False):
+        game_start_variable,player1_hero_choice,player2_hero_choice,start = MAIN_menu.create_menu(screen,game_start_variable)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_start_variable = False
+    if start:
+        if create_level:
+            player1_hero = [all_animations[heroes_dico[player1_hero_choice][0]], heroes_dico[player1_hero_choice][1], heroes_dico[player1_hero_choice][2], player1_hero_choice, heroes_dico[player1_hero_choice][3]]
+            player2_hero = [all_animations[heroes_dico[player2_hero_choice][0]], heroes_dico[player2_hero_choice][1], heroes_dico[player2_hero_choice][2], player2_hero_choice, heroes_dico[player2_hero_choice][3]]
+            level = Level(level_map, screen, player1_hero, player2_hero)
+            create_level = False
+        screen.fill((255, 255, 255))
+        level.run()
+        display_fps()
+    clock.tick(60)
     pygame.display.update()
-    clock.tick(FPS)
